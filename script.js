9ca8eb64050432cc206ca8884736739a7e5b4667
rodriguez <rodisenrique73@gmail.com>
2023-01-22 10:33:32 -0500

    update

--- script.js
+++ script.js
@@ -64,14 +64,13 @@ function copiarTxt() {
   let copiatxt = output_text.value;
   navigator.clipboard.writeText(copiatxt);
   alert("Texto copiado");
- // clear();
+  clear();
 
 }
 
-/*
 function clear() {
   output_text.value = "";
   input_text.value = "";
   mensaje.hidden = false;
   copiar.style.visibility = "hidden";
-}*/
+}
